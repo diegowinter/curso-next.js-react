fba18c698c07d69820c1332fafd64c83b64a6a33
Diego Winter <diego@diegowinter.dev>
2022-03-12 17:01:11 -0300

    propriedade componente

--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -1,9 +1,17 @@
 import Titulo from "../../components/Titulo"
 
 export default function usandoTitulo() {
-    return (
-        <div>
-            <Titulo />
-        </div>
-    )
+  return (
+    <div>
+      <Titulo
+        principal='Página de cadastro grande'
+        secundario='Incluir, alterar e excluir coisas...'
+      />
+      <Titulo
+        principal='Página de cadastro pequena'
+        secundario='Incluir, alterar e excluir coisas...'
+        pequeno={true} // ou só o nome pequeno, pois a existencia do parâmetro já é true
+      />
+    </div>
+  )
 }
