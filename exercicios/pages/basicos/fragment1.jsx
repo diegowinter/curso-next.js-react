3ee387f72db06323f095d9e823e2f4c6794c0611
Diego Winter <diego@diegowinter.dev>
2022-03-12 16:38:24 -0300

    fragment

---
+++ exercicios/pages/basicos/fragment1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function fragment() {
+    return (
+        <React.fragment>
+            <h1>Titulo</h1>
+            <h1>Subtitulo</h1>
+        </React.fragment>
+    )
+}
---
+++ exercicios/pages/basicos/fragment2.jsx
@@ -0,0 +1,8 @@
+export default function fragment() {
+    return (
+        <>
+            <h1>Titulo</h1>
+            <h1>Subtitulo</h1>
+        </>
+    )
+}
