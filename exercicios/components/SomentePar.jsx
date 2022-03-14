74fa4e4fadaea67a74171b4ca4650cdf8f3a137b
Diego Winter <diego@diegowinter.dev>
2022-03-14 01:57:33 -0300

    condicional #01

---
+++ exercicios/components/SomentePar.jsx
@@ -0,0 +1,16 @@
+export default function SomentePar(props) {
+    // return props.numero % 2 === 0 ? <span>{props.numero}</span> : null
+
+    const numeroPar = props.numero % 2 === 0
+    return (
+        <div>
+            {numeroPar ? <span>{props.numero}</span> : null}
+        </div>
+    )
+
+    // if (props.numero % 2 === 0) {
+    //     return <span>{props.numero}</span>
+    // } else {
+    //     return null
+    // }
+}
