219cdd28ef19ac596b0f284dbd2429824ca0e527
Diego Winter <diego@diegowinter.dev>
2022-05-10 05:06:38 -0300

    estrutura do projeto

--- next-basico/next.config.js
+++ next-basico/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    // {process.env.dbPath}
+    dbPath: 'mysql... (ex. de variável de ambiente)'
+  }
 }
 
 module.exports = nextConfig
