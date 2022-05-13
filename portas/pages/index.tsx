0ee0fc19ec5c6122650a4522bed39ae0a09c9e47
Diego Winter <diego@diegowinter.dev>
2022-05-13 11:39:46 -0300

    estruturando formulário

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,8 +1,24 @@
-import Jogo from "./jogo";
+import Cartao from "../components/Cartao";
+import styles from '../styles/Formulario.module.css'
+import Link from "next/link";
 
-export default function Home() {
-
+export default function Formulario() {
   return (
-    <h1>Início!</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor='#c0392c' >
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao />
+      </div>
+      <div>
+        <Cartao />
+        <Cartao bgcolor='#28a085' >
+          <Link href={`jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+    </div>
   )
 }
