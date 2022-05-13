223daa2cd00f9e6086bed215cea79ac72f58c695
Diego Winter <diego@diegowinter.dev>
2022-05-13 10:43:17 -0300

    componente jogo

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,23 +1,8 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
-import PortaModel from "../model/Porta";
+import Jogo from "./jogo";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta
-        key={porta.numero}
-        value={porta}
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
 
   return (
-    <div style={{ display: 'flex' }}>
-      {renderizarPortas()}
-    </div>
+    <h1>Início!</h1>
   )
 }
