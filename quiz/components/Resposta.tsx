026f5d6c4b61cbcd016b034e3a5bebd10a413d80
Diego Winter <diego@diegowinter.dev>
2022-05-19 11:10:35 -0300

    componente Resposta #01 e #02

---
+++ quiz/components/Resposta.tsx
@@ -0,0 +1,34 @@
+import RespostaModel from '../model/resposta'
+import styles from '../styles/Resposta.module.css'
+
+interface RespostaProps {
+  valor: RespostaModel,
+  indice: number,
+  letra: string,
+  corFundoLetra: string,
+}
+
+const Resposta = (props: RespostaProps) => {
+  const resposta = props.valor
+
+  return (
+    <div className={styles.resposa}>
+      <div className={styles.conteudoResposta}>
+        <div className={styles.frente}>
+          <div className={styles.letra}
+            style={{ backgroundColor: props.corFundoLetra }}>
+            {props.letra}
+          </div>
+          <div className={styles.valor}>
+            {resposta.valor}
+          </div>
+        </div>
+        <div className={styles.verso}>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Resposta;
