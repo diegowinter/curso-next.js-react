6d3de12e1ca0413ef7796a14b836fa26cff267fc
Diego Winter <diego@diegowinter.dev>
2022-05-19 13:21:45 -0300

    componente Botão

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import Botao from "../components/Botao";
 import Questao from "../components/Questao";
 import QuestaoModel from "../model/questao";
 import RespostaModel from "../model/resposta";
@@ -31,6 +32,7 @@ export default function Home() {
   return (
     <div style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh'
@@ -39,6 +41,7 @@ export default function Home() {
         respostaFornecida={respostaFornecida}
         tempoPraResposta={5}
         tempoEsgotado={tempoEsgotado} />
+      <Botao texto={'Próxima'} href='/resultado' />
     </div>
   )
 }
