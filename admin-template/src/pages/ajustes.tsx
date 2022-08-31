27fa042acee680d0b7795ecca3a00806f1795d63
Diego Winter <diego@diegowinter.dev>
2022-08-31 14:40:50 -0300

    componente menu lateral 2

---
+++ admin-template/src/pages/ajustes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout titulo="Ajustes" subtitulo="Aqui você faz os ajustes" >
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
