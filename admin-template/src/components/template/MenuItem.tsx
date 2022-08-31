3b0631c9e1864c8990f690077740f8f0ff4d39af
Diego Winter <diego@diegowinter.dev>
2022-08-31 15:00:28 -0300

    componente menu lateral 3

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,25 +1,37 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-  url: string
   texto: string
   icone: any
+  url?: string
+  className?: string
+  onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
-  return (
-    <li className={`hover:bg-gray-100`}>
-      <Link href={props.url}>
-        <a className={`
+  function renderizarLink() {
+    return (
+      <a className={`
           flex flex-col justify-center items-center
-          h-20 w-20
+          h-20 w-20 text-gray-600 ${props.className}
         `}>
-          {props.icone}
-          <span className={`
-            text-xs font-light text-gray-600
+        {props.icone}
+        <span className={`
+            text-xs font-light 
           `}>{props.texto}</span>
-        </a>
-      </Link>
+      </a>
+    )
+  }
+
+  return (
+    <li onClick={props.onClick} className={`hover:bg-gray-100 cursor-pointer`}>
+      {props.url ? (
+        <Link href={props.url}>
+          {renderizarLink()}
+        </Link>
+      ) : (
+        renderizarLink()
+      )}
     </li>
   )
 }
