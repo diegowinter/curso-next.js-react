b73120cd5f70c2e397edb8e68607a4181bc50756
Diego Winter <diego@diegowinter.dev>
2022-08-31 14:24:34 -0300

    esquema de cores

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -14,7 +14,10 @@ export default function Layout(props: LayoutProps) {
       flex h-screen w-screen
     `}>
       <MenuLateral />
-      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+      <div className={`
+          flex flex-col w-full p-7
+          bg-gray-300 dark:bg-gray-800
+        `}>
         <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
         <Conteudo>{props.children}</Conteudo>
       </div>
