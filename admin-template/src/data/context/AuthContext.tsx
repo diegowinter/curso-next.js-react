239c47f37fe2dc07cfbf259fbf5d8840db19d785
Diego Winter <diego@diegowinter.dev>
2022-09-01 17:53:25 -0300

    criando sessão com cookie

--- admin-template/src/data/context/AuthContext.tsx
+++ admin-template/src/data/context/AuthContext.tsx
@@ -1,7 +1,8 @@
 import router from 'next/router'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import firebase from '../../firebase/config'
 import Usuario from '../../model/Usuario'
+import Cookies from 'js-cookie'
 
 interface AuthContextProps {
   usuario?: Usuario
@@ -25,19 +26,53 @@ async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuar
 }
 
 export function AuthProvider(props: any) {
+  const [carregando, setCarregando] = useState(true)
   const [usuario, setUsuario] = useState<Usuario>()
+
+  async function configurarSessao(usuarioFirebase: firebase.User) {
+    if (usuarioFirebase?.email) {
+      const usuario = await usuarioNormalizado(usuarioFirebase)
+      setUsuario(usuario)
+      gerenciarCookie(true)
+      setCarregando(false)
+
+      return usuario.email
+    } else {
+      setUsuario(null!)
+      gerenciarCookie(false)
+      setCarregando(false)
+
+      return false
+    }
+  }
 
   async function loginGoogle() {
     const resp = await firebase.auth().signInWithPopup(
       new firebase.auth.GoogleAuthProvider()
     )
 
-    if (resp.user?.email) {
-      const usuario = await usuarioNormalizado(resp.user!)
-      setUsuario(usuario)
-      router.push('/')
+    // if (resp.user?.email) {
+    //   const usuario = await usuarioNormalizado(resp.user!)
+    //   setUsuario(usuario)
+    // }
+    await configurarSessao(resp.user!)
+    router.push('/')
+  }
+
+  function gerenciarCookie(logado: boolean) {
+    if (logado) {
+      Cookies.set('admin-template-cod3r-auth', logado.toString(), {
+        expires: 7
+      })
+    } else {
+      Cookies.remove('admin-template-cod3r-auth')
     }
   }
+
+  useEffect(() => {
+    const cancelar = firebase.auth().onIdTokenChanged(configurarSessao)
+    return () => cancelar()
+  }, [])
 
   return (
     <AuthContext.Provider value={{
