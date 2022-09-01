eb0a9f6e94ccf4957c56422e4c3fe194f4b10781
Diego Winter <diego@diegowinter.dev>
2022-09-01 16:15:00 -0300

    tela autenticação 2

--- admin-template/src/components/auth/AuthInput.tsx
+++ admin-template/src/components/auth/AuthInput.tsx
@@ -9,12 +9,18 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return props.naoRenderizarQuando ? null : (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-4">
       <label>{props.label}</label>
       <input
         type={props.tipo ?? 'text'}
         value={props.valor}
         required={props.obrigatorio}
+        className={`
+          px-4 py-3 rounded-lg bg-gray-200 mt-2
+          border focus:border-blue-500 focus:bg-white
+          focus:outline-none
+        `}
+        onChange={(e) => props.valorMudou(e.target.value)}
       />
     </div>
   )
